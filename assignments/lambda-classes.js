c9052dc04aab4c7cdce46afb6fe525b426f201ff
Orie Chinedu <oriechinedu@gmail.com>
2019-05-09 13:14:18 +0100

    Implement Person and Instructor classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,39 @@
 // CODE here for your Lambda Classes
+
+class Person {
+  constructor(props) {
+    const { 
+      name, 
+      age, 
+      location,
+      gender
+     } = props;
+     this.name = name;
+     this.age= age;
+     this.location = location;
+     this.gender = gender;
+  }
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}`
+  }
+}
+
+class Instructor extends Person {
+  constructor(props) {
+    super(props)
+    const { 
+      specialty,
+      favLanguage,
+      catchPhrase
+    } = props;
+    this.specialty = specialty;
+    this.favLanguage = favLanguage;
+    this.catchPhrase = catchPhrase; 
+  }
+  demo(subject) {
+    return `Today we are learning about ${subject}`
+  }
+  grade(student, subject) {
+    return `${student.name} receives a perfect score on ${subject}`;
+  }
+}
